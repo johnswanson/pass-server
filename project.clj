7817d90a24c3b0131bca3d23b1f5057407f42277
John Swanson <john.d.swanson@gmail.com>
2014-10-09 10:20:40 -0700

    update dependencies

--- project.clj
+++ project.clj
@@ -34,7 +34,7 @@
                  [hiccup "1.0.5"]
                  [fogus/ring-edn "0.2.0"]
                  [environ "1.0.0"]
-                 [ring/ring-core "1.2.2"]
-                 [ring/ring-jetty-adapter "1.2.2"]
+                 [ring/ring-core "1.3.1"]
+                 [ring/ring-jetty-adapter "1.3.1"]
                  [ring-server "0.3.1"]
-                 [compojure "1.1.6"]])
+                 [compojure "1.2.0"]])
